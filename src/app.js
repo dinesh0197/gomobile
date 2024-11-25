38320bae571178c38eab7fef203e8ed306160682
dinesh <dineshr0197@gmail.com>
2024-11-25 23:31:25 +0530

    removed the fucking shit

--- src/app.js
+++ src/app.js
@@ -70,22 +70,22 @@ app.use(globalErrorController);
 
 module.exports = app;
 
-(async ()=>{
-  await User.create({
-   email: "kash@yopmail.com",
-   password: "kash@yopmail.com",
-   franchise_code: "12345",
-   operating_name: "123",
-   legal_name: "legal_name",
-   phone: "7788654387",
-   address: "address",
-   city: "city",
-   state: "state",
-   country: "country",
-   zipcode: "zipcode",
-   role:  "Admin", // Default role if none provided
-   created_by: "system", // Adjust as necessary
-   createdAt: new Date(),
-   updatedAt: new Date(),
- });
-})()
+// (async ()=>{
+//   await User.create({
+//    email: "kash@yopmail.com",
+//    password: "kash@yopmail.com",
+//    franchise_code: "12345",
+//    operating_name: "123",
+//    legal_name: "legal_name",
+//    phone: "7788654387",
+//    address: "address",
+//    city: "city",
+//    state: "state",
+//    country: "country",
+//    zipcode: "zipcode",
+//    role:  "Admin", // Default role if none provided
+//    created_by: "system", // Adjust as necessary
+//    createdAt: new Date(),
+//    updatedAt: new Date(),
+//  });
+// })()
